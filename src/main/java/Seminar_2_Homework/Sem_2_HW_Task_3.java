944a1654325329504e5f95a6ce4fd7e2f28fe259
Sanders76 <sanders76@list.ru>
2023-04-04 23:25:12 +0300

    Added code to Sem_2_HW_Task_3.java

--- src/main/java/Seminar_2_Homework/Sem_2_HW_Task_3.java
+++ src/main/java/Seminar_2_Homework/Sem_2_HW_Task_3.java
@@ -1,7 +1,40 @@
 package Seminar_2_Homework;
 
+import java.io.File;
+
 public class Sem_2_HW_Task_3 {
     public static void main(String[] args) {
 
+        // Задача 3*.Напишите метод, который определит тип (расширение)
+        // файлов из текущей папки и выведет в консоль результат вида:
+        // 1 Расширение файла: txt
+        // 2 Расширение файла: pdf
+        // 3 Расширение файла:
+        // 4 Расширение файла: jpg
+
+        CatchFileExtentions("src/main/resources/Files");
+    }
+
+
+    private static void CatchFileExtentions(String pathDir) {
+        File file = new File(pathDir);
+        if (!file.isDirectory()) {
+            return;
+        }
+
+        String[] fileNamesInFolder = file.list();
+
+        StringBuilder allFileNamesInFolder = new StringBuilder();
+        for (String fileName : fileNamesInFolder) {
+            allFileNamesInFolder.append(fileName).append(System.lineSeparator());
+        }
+
+        System.out.println("В заданной папке находятся файлы со следующими расширениями:");
+        for (int i = 0; i < fileNamesInFolder.length ; i++) {
+            String temp = fileNamesInFolder[i];
+            //System.out.println(temp);
+            String[] arr = temp.split("\\.");
+            System.out.printf(".%s%n", arr[1]);
+        }
     }
 }
