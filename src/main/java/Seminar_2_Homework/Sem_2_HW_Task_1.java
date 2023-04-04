40ae0c5dc807ef4f12d4a14c2974ebb726a05e39
Sanders76 <sanders76@list.ru>
2023-04-04 22:36:55 +0300

    Added changes to code of Sem_2_HW_Task_1.java

--- src/main/java/Seminar_2_Homework/Sem_2_HW_Task_1.java
+++ src/main/java/Seminar_2_Homework/Sem_2_HW_Task_1.java
@@ -1,41 +1,56 @@
 package Seminar_2_Homework;
 
+import java.util.Arrays;
+
 public class Sem_2_HW_Task_1 {
     public static void main(String[] args) {
         // 1. Напишите метод, который принимает на вход строку (String)
         // и определяет является ли строка палиндромом (возвращает boolean значение).
 
         // String str = "fhgidifikfdsosoaol";
-        String str = "fhgiddighf";
-        // String str = "fhgidadighf";
+        // String str = "fhgiddighf";
+        String str = "fhgidadighf";
 
-        if (isPalindrom(str)) {
-            System.out.println("Cтрока является палиндромом");
+        // Version 1
+
+        if (isPalindromVers1(str)) {
+            System.out.println("V1: Cтрока является палиндромом");
         } else {
-            System.out.println("Cтрока не является палиндромом");
+            System.out.println("V1: Cтрока не является палиндромом");
+        }
+
+        // Version 2
+
+        if (isPalindromVers2(str)) {
+            System.out.println("V2: Cтрока является палиндромом");
+        } else {
+            System.out.println("V2: Cтрока не является палиндромом");
         }
     }
 
-    private static boolean isPalindrom(String str) {
+
+    // Version 1. Метод проверки на палиндром через сравнение элементов массива
+    private static boolean isPalindromVers1(String str) {
         char[] charStringArr = str.toCharArray();
-        if (charStringArr.length % 2 == 0) {
-            for (int i = 0; i < charStringArr.length / 2; i++) {
-                //int indFromEnd = charStringArr.length - 1;
-                if (charStringArr[i] == charStringArr[(charStringArr.length - 1) - i]) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        } else {
-            for (int i = 0, indFromEnd = charStringArr.length - 1; i < ((charStringArr.length - 1) / 2); i++, indFromEnd--) {
-                if (charStringArr[i] == charStringArr[indFromEnd]) {
-                    return true;
-                } else {
-                    return false;
-                }
+        for (int i = 0; i < charStringArr.length / 2; i++) {
+            if (charStringArr[i] != charStringArr[(charStringArr.length - 1) - i]) {
+                return false;
             }
         }
+        return true;
+    }
 
+
+    // Version 2. Метод проверки на палиндром через разворот массива
+    private static boolean isPalindromVers2(String str) {
+        char[] charStringArr = str.toCharArray();
+        char[] reversedArr = new char[charStringArr.length];
+        for (int i = 0; i <= charStringArr.length - 1; i++) {
+            reversedArr[i] = charStringArr[(charStringArr.length - 1) - i];
+        }
+        // System.out.println(charStringArr);
+        // System.out.println(reversedArr);
+        boolean areArraysEqual = Arrays.equals(charStringArr, reversedArr);
+        return areArraysEqual;
     }
 }
